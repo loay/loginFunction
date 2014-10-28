fefcb2c90136f2dafeb2ae93357866fcc861afe2
Loay Gewily <loay@users.noreply.github.com>
2014-10-28 01:25:35 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,8 @@
 <?php
+ob_start();
 include 'connect_to_database.php'; //connect the connection page
 if(empty($_SESSION)) // if the session not yet started 
-//ini_set('session.save_path', 'tmp');
+ini_set('session.save_path', 'tmp');
    session_start();
 
 
@@ -10,15 +11,30 @@ if(isset($_SESSION['username'])) { // if already login
    exit; 
 }
 
-
+ob_flush();
 ?>
 <html>
-<head></head>
+<head>
+<title>Login Page</title>
+
+</head>
 <body>
+<br />
+<br />
+<br />
+<br />
+<p align="center">
+<table border="0"> <tr> <th> </th> <th>  </th> <th>  </th> <th></th> </tr>
+<tr><td colspan="2" rowspan="3"><img src="logo.png" alt="welcome image" width="250" height="250"></td>
+<td>
 <form action = "login_proccess.php" method = "post">
-Username: <input type="text" name="username" /><br />
-Password: <input type="password" name="password" /><br />
-<input type = "submit" name="submit" value="login" />
+Username: </td><td><input type="text" name="username" /> </td></tr>
+<tr><td>Password: </td><td><input type="password" name="password" /> </td></tr>
+<tr><td colspan="2" align="center"><input class="button" type = "submit" name="submit" value="Login" /></td></tr>
+<tr><td> Create an account:</td><td colspan="3" align="center"><a href="register.php" class="button">Register</a></td></tr> 
 </form>
+</td></tr></table>
+</p>
+
 </body>
 </html>
