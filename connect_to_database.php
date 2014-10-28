a144d94678569f5a8b252462365ab6732e166323
Loay Gewily <loay@users.noreply.github.com>
2014-10-28 01:21:28 -0400

    Update connect_to_database.php

--- connect_to_database.php
+++ connect_to_database.php
@@ -1,9 +1,15 @@
 <?php
-mysql_connect("localhost","username","password") or die(mysql_error()); 
-mysql_select_db("databaseName");
 
-//or if it is not localhost then it could be like that
 
-//mysql_connect("mysql.freehosting.com","username","password") or die(mysql_error()); 
-//mysql_select_db("databaseName");
+$db_host='localhost'; //enter db host
+$db_name='name'; //enter db name
+$db_username='username';  //enter db username
+$db_password='password'; //enter db password
+
+
+$connect1=mysql_connect($db_host,$db_username,$db_password) or die(mysql_error()); 
+	if (!$connect1) {
+    die('Could not connect: ' . mysql_error());
+	}
+mysql_select_db($db_name,$connect1);
 ?> 
